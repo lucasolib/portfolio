0af0095117b66b42f36351ff2533f6df9b53affd
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2023-09-12 21:08:36 -0300

    Fix: images broken

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -6,27 +6,27 @@ function Contact() {
       <ul className="socialMediaButtons">
         <li>
           <a href="https://github.com/lucasolib" target="_blank" rel="noreferrer">
-            <img src="../public/githubLogo.svg" alt="github logo" />
+            <img src="https://i.imgur.com/HIH1JNg.png" alt="github logo" />
           </a>
         </li>
         <li>
           <a href="https://instagram.com/lucasthbatista" target="_blank" rel="noreferrer">
-            <img src="../public/instagramLogo.svg" alt="instagram logo" />
+            <img src="https://i.imgur.com/Qvf748k.png" alt="instagram logo" />
           </a>
         </li>
         <li>
           <a href="https://www.linkedin.com/in/lucas-de-oliveira-dev/" target="_blank" rel="noreferrer">
-            <img src="../public/linkedinLogo.svg" alt="linkedin logo" />
+            <img src="https://i.imgur.com/Lf6Q1rv.png" alt="linkedin logo" />
           </a>
         </li>
         <li>
           <a href="mailto:lucasolibatista@gmail.com" target="_blank" rel="noreferrer">
-            <img src="../public/emailLogo.svg" alt="email logo" />
+            <img src="https://i.imgur.com/yTJq0yC.png" alt="email logo" />
           </a>
         </li>
         <li>
           <a href="https://wa.me/5522996089686" target="_blank" rel="noreferrer">
-            <img src="../public/whatsappLogo.svg" alt="whatsapp logo" />
+            <img src="https://i.imgur.com/56doQic.png" alt="whatsapp logo" />
           </a>
         </li>
       </ul>
