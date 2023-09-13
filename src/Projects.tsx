e31cb8e6c63a2ade60fc248ab97079644db34d96
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2023-09-12 21:09:21 -0300

    Fix: images from projects

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -6,37 +6,37 @@ function Projects() {
       <h1 className="projectTitle">Projetos</h1>
       <ul className="projects">
         <li>
-          <img src="../public/soonImg.svg" alt="Em breve" className="soonImg" />
+          <img src="https://i.imgur.com/hsg3wCj.png" alt="Em breve" className="soonImg" />
           <p className="description">Nome: Em breve</p>
           <p className="description">GitHub: Em breve</p>
           <p className="description">Projeto: Em breve</p>
         </li>
         <li>
-          <img src="../public/soonImg.svg" alt="Em breve" className="soonImg" />
+          <img src="https://i.imgur.com/hsg3wCj.png" alt="Em breve" className="soonImg" />
           <p className="description">Nome: Em breve</p>
           <p className="description">GitHub: Em breve</p>
           <p className="description">Projeto: Em breve</p>
         </li>
         <li>
-          <img src="../public/soonImg.svg" alt="Em breve" className="soonImg" />
+          <img src="https://i.imgur.com/hsg3wCj.png" alt="Em breve" className="soonImg" />
           <p className="description">Nome: Em breve</p>
           <p className="description">GitHub: Em breve</p>
           <p className="description">Projeto: Em breve</p>
         </li>
         <li>
-          <img src="../public/soonImg.svg" alt="Em breve" className="soonImg" />
+          <img src="https://i.imgur.com/hsg3wCj.png" alt="Em breve" className="soonImg" />
           <p className="description">Nome: Em breve</p>
           <p className="description">GitHub: Em breve</p>
           <p className="description">Projeto: Em breve</p>
         </li>
         <li>
-          <img src="../public/soonImg.svg" alt="Em breve" className="soonImg" />
+          <img src="https://i.imgur.com/hsg3wCj.png" alt="Em breve" className="soonImg" />
           <p className="description">Nome: Em breve</p>
           <p className="description">GitHub: Em breve</p>
           <p className="description">Projeto: Em breve</p>
         </li>
         <li>
-          <img src="../public/soonImg.svg" alt="Em breve" className="soonImg" />
+          <img src="https://i.imgur.com/hsg3wCj.png" alt="Em breve" className="soonImg" />
           <p className="description">Nome: Em breve</p>
           <p className="description">GitHub: Em breve</p>
           <p className="description">Projeto: Em breve</p>
