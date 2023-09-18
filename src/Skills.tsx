169a57a5b3563d2b53b67cafed4de9d135b43155
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2023-09-18 16:12:58 -0300

    Feat: Adicionado imagem de background na seção de habilidades

--- src/Skills.tsx
+++ src/Skills.tsx
@@ -1,16 +1,34 @@
 import './css/Skills.css';
-import { useState } from 'react';
+import { useState, useEffect, useMemo } from 'react';
+import ISkills from './interfaces/skills';
 
 function Skills() {
   const skills = ['git', 'github', 'html', 'css', 'js', 'ts', 'jest', 'react', 'redux',
     'docker', 'express', 'mysql', 'sequelize', 'node', 'mongodb', 'python'];
-  // const skillsImgs = {
-  //   git: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg',
-  //   github: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg',
-  //   html: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg',
-  //   css: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg',
-  // };
-  const [selectedSkill, setSelectedSkill] = useState(skills[0]);
+  const skillsImgs: ISkills = useMemo(() => ({
+    git: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg',
+    github: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg',
+    html: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg',
+    css: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg',
+    js: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+    ts: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+    jest: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg',
+    react: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg',
+    redux: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg',
+    docker: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain-wordmark.svg',
+    express: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg',
+    mysql: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain-wordmark.svg',
+    sequelize: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original-wordmark.svg',
+    node: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg',
+    mongodb: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg',
+    python: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg',
+  }), []);
+  const [selectedSkill, setSelectedSkill] = useState<string>(skills[0]);
+  const [renderSelectedIcon, setRenderSelectedIcon] = useState<string>(skillsImgs.git);
+
+  useEffect(() => {
+    setRenderSelectedIcon(skillsImgs[selectedSkill]);
+  }, [selectedSkill, skillsImgs]);
 
   const nextSkill = () => {
     const index = skills.indexOf(selectedSkill);
@@ -33,112 +51,112 @@ function Skills() {
         <div className="iconsContainer">
           <div className="outsideIcon">
             <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg"
+              src={ skillsImgs.git }
               alt="Git Icon"
               className="icons"
             />
           </div>
           <div className="outsideIcon">
             <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg"
+              src={ skillsImgs.github }
               alt="GitHub Icon"
               className="icons"
             />
           </div>
           <div className="outsideIcon">
             <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg"
+              src={ skillsImgs.html }
               alt="HTML5 Icon"
               className="icons"
             />
           </div>
           <div className="outsideIcon">
             <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg"
+              src={ skillsImgs.css }
               alt="CSS3 Icon"
               className="icons"
             />
           </div>
           <div className="outsideIcon">
             <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+              src={ skillsImgs.js }
               alt="JavaScript Icon"
               className="icons"
             />
           </div>
           <div className="outsideIcon">
             <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+              src={ skillsImgs.ts }
               alt="TypeScript Icon"
               className="icons"
             />
           </div>
           <div className="outsideIcon">
             <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg"
+              src={ skillsImgs.jest }
               alt="Jest Icon"
               className="icons"
             />
           </div>
           <div className="outsideIcon">
             <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+              src={ skillsImgs.react }
               alt="React Icon"
               className="icons"
             />
           </div>
           <div className="outsideIcon">
             <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg"
+              src={ skillsImgs.redux }
               alt="Redux Icon"
               className="icons"
             />
           </div>
           <div className="outsideIcon">
             <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain-wordmark.svg"
+              src={ skillsImgs.docker }
               alt="Docker Icon"
               className="icons"
             />
           </div>
           <div className="outsideIcon">
             <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg"
+              src={ skillsImgs.express }
               alt="Express Icon"
               className="icons"
             />
           </div>
           <div className="outsideIcon">
             <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain-wordmark.svg"
+              src={ skillsImgs.mysql }
               alt="MySQL Icon"
               className="icons"
             />
           </div>
           <div className="outsideIcon">
             <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original-wordmark.svg"
+              src={ skillsImgs.sequelize }
               alt="Sequelize Icon"
               className="icons"
             />
           </div>
           <div className="outsideIcon">
             <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg"
+              src={ skillsImgs.node }
               alt="NodeJS Icon"
               className="icons"
             />
           </div>
           <div className="outsideIcon">
             <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"
+              src={ skillsImgs.mongodb }
               alt="MongoDB Icon"
               className="icons"
             />
           </div>
           <div className="outsideIcon">
             <img
-              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg"
+              src={ skillsImgs.python }
               alt="Python Icon"
               className="icons"
             />
@@ -160,7 +178,19 @@ function Skills() {
             <span className="arrow" />
           </button>
         </div>
-        <p className="skillsText">Futuro texto sobre descrição das Skills</p>
+        <div className="descriptionSkill">
+          <img
+            src={ renderSelectedIcon }
+            alt="Selected skill Icon"
+            className="backgroundIcon"
+          />
+          <p className="skillsText">
+            Git é um sistema de controle de
+            versão distribuído que permite aos desenvolvedores rastrear
+            e gerenciar mudanças no código fonte de um projeto, facilitando
+            a colaboração e a manutenção do software.
+          </p>
+        </div>
       </div>
     </div>
   );
---
+++ src/interfaces/skills.ts
@@ -0,0 +1,21 @@
+interface Skills {
+  [key: string]: string;
+  git: string,
+  github: string,
+  html: string,
+  css: string,
+  js: string,
+  ts: string,
+  jest: string,
+  react: string,
+  redux: string,
+  docker: string,
+  express: string,
+  mysql: string,
+  sequelize: string,
+  node: string,
+  mongodb: string,
+  python: string,
+}
+
+export default Skills;
