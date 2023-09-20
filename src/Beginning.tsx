0948e2dd5df687357f48096d5e0ac839bfaa2054
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2023-09-20 11:29:37 -0300

    Style: Muda frase inicial

--- src/Beginning.tsx
+++ src/Beginning.tsx
@@ -3,10 +3,9 @@ import './css/Beginning.css';
 function Beginning() {
   return (
     <div className="beginningPage" id="beginning">
-      <h1 className="beginningTitle">Olá e bem vindo ao meu portfólio.</h1>
+      <h1 className="beginningTitle">Olá, seja bem vindo ao meu portfólio.</h1>
       <h2 className="beginningText">
-        Me chamo Lucas e essa é
-        a versão 0.2 do meu porfólio!
+        Me chamo Lucas e sou desenvolvedor web fullstack.
       </h2>
     </div>
   );
