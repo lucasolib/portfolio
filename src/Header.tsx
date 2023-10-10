feeeb775f80c50937961f31393ab0148557aa65d
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2023-10-09 22:41:39 -0300

    Style: Header to mobile

--- src/Header.tsx
+++ src/Header.tsx
@@ -26,21 +26,27 @@ function Header() {
         alt="portfolioLogo"
         src="https://i.imgur.com/GFyTxKs.png"
       />
-      <a className="link" href="#beginning">
-        <p>Início</p>
-      </a>
-      <a className="link" href="#about">
-        <p>Sobre mim</p>
-      </a>
-      <a className="link" href="#skills">
-        <p>Habilidades</p>
-      </a>
-      <a className="link" href="#project">
-        <p>Projetos</p>
-      </a>
-      <a className="link" href="#contact">
-        <p>Contatos</p>
-      </a>
+      <nav className="dropDown">
+        {/* <img
+          src="https://static.thenounproject.com/png/356889-200.png"
+          alt="menuButton"
+        /> */}
+        <a className="link" href="#beginning">
+          <p>Início</p>
+        </a>
+        <a className="link" href="#about">
+          <p>Sobre mim</p>
+        </a>
+        <a className="link" href="#skills">
+          <p>Habilidades</p>
+        </a>
+        <a className="link" href="#project">
+          <p>Projetos</p>
+        </a>
+        <a className="link" href="#contact">
+          <p>Contatos</p>
+        </a>
+      </nav>
     </div>
   );
 }
