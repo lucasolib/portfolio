d7e2d76ed3d7b993789067990a29c449d134c7db
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2023-10-10 14:22:02 -0300

    refactor: projects responsive render

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,46 +1,21 @@
 import './css/Project.css';
 
 function Projects() {
+  const projects = ['soon', 'soon', 'soon', 'soon', 'soon', 'soon'];
   return (
     <div className="projectPage" id="project">
       <h1 className="projectTitle">Projetos</h1>
       <ul className="projects">
-        <li>
-          <img src="https://i.imgur.com/hsg3wCj.png" alt="Em breve" className="soonImg" />
-          <p className="description">Nome: Em breve</p>
-          <p className="description">GitHub: Em breve</p>
-          <p className="description">Projeto: Em breve</p>
-        </li>
-        <li>
-          <img src="https://i.imgur.com/hsg3wCj.png" alt="Em breve" className="soonImg" />
-          <p className="description">Nome: Em breve</p>
-          <p className="description">GitHub: Em breve</p>
-          <p className="description">Projeto: Em breve</p>
-        </li>
-        <li>
-          <img src="https://i.imgur.com/hsg3wCj.png" alt="Em breve" className="soonImg" />
-          <p className="description">Nome: Em breve</p>
-          <p className="description">GitHub: Em breve</p>
-          <p className="description">Projeto: Em breve</p>
-        </li>
-        <li>
-          <img src="https://i.imgur.com/hsg3wCj.png" alt="Em breve" className="soonImg" />
-          <p className="description">Nome: Em breve</p>
-          <p className="description">GitHub: Em breve</p>
-          <p className="description">Projeto: Em breve</p>
-        </li>
-        <li>
-          <img src="https://i.imgur.com/hsg3wCj.png" alt="Em breve" className="soonImg" />
-          <p className="description">Nome: Em breve</p>
-          <p className="description">GitHub: Em breve</p>
-          <p className="description">Projeto: Em breve</p>
-        </li>
-        <li>
-          <img src="https://i.imgur.com/hsg3wCj.png" alt="Em breve" className="soonImg" />
-          <p className="description">Nome: Em breve</p>
-          <p className="description">GitHub: Em breve</p>
-          <p className="description">Projeto: Em breve</p>
-        </li>
+        {projects.map((project, i) => {
+          return (
+            <li key={ i }>
+              <img src="https://i.imgur.com/hsg3wCj.png" alt="Em breve" className="soonImg" />
+              <p className="description">Nome: Em breve</p>
+              <p className="description">GitHub: Em breve</p>
+              <p className="description">Projeto: Em breve</p>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
